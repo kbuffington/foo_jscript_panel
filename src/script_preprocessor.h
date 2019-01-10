2b0b39647f1417d8b341575deb5bbfedfc9a310c
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-01-10 20:12:37 +0000

    Update t_script_info again!

--- src/script_preprocessor.h
+++ src/script_preprocessor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "helpers.h"
 
 class host_comm;
 
@@ -15,24 +16,17 @@ struct t_directive_value
 
 struct t_script_info
 {
-	bool dragdrop;
-	pfc::string_list_impl imports;
-	pfc::string8_fast name;
-	pfc::string8_fast version;
-	pfc::string8_fast author;
+	t_script_info(GUID& guid_ref) : m_guid_ref(guid_ref) {}
 
-	t_script_info(GUID& guid_ref) : m_guid_ref(guid_ref)
+	struct
 	{
-	}
-
-	void clear()
-	{
-		imports.remove_all();
-		dragdrop = false;
-		name = "";
-		version = "";
-		author = "";
-	}
+		std::string which;
+		std::string path;
+	} expand_table[3] = {
+		{ "%fb2k_path%", helpers::get_fb2k_path().get_ptr() },
+		{ "%fb2k_component_path%", helpers::get_fb2k_component_path().get_ptr() },
+		{ "%fb2k_profile_path%", helpers::get_profile_path().get_ptr() },
+	};
 
 	pfc::string8_fast build_info_string() const
 	{
@@ -58,6 +52,35 @@ struct t_script_info
 		ret << ")";
 		return ret;
 	}
+
+	pfc::string8_fast expand_import(t_size idx)
+	{
+		std::string tmp = imports[idx];
+		for (t_size i = 0; i < _countof(expand_table); ++i)
+		{
+			if (tmp.find(expand_table[i].which) == 0)
+			{
+				tmp = expand_table[i].path + tmp.substr(expand_table[i].which.length());
+				break;
+			}
+		}
+		return tmp.c_str();
+	}
+
+	void clear()
+	{
+		imports.remove_all();
+		dragdrop = false;
+		name = "";
+		version = "";
+		author = "";
+	}
+
+	bool dragdrop;
+	pfc::string_list_impl imports;
+	pfc::string8_fast name;
+	pfc::string8_fast version;
+	pfc::string8_fast author;
 
 private:
 	GUID& m_guid_ref;
