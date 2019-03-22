21bd0f1490a742197e4d5bb286e4740f4781603a
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-03-22 11:45:08 +0000

    Tidy up config

--- src/ui/config.cpp
+++ src/ui/config.cpp
@@ -7,7 +7,7 @@ bool prop_kv_config::get_config_item(const char* p_key, VARIANT& p_out)
 
 	if (m_map.query(p_key, val))
 	{
-		if (g_is_allowed_type(val.vt) && SUCCEEDED(VariantCopy(&p_out, &val)))
+		if (g_get_sizeof(val.vt) != 0 && SUCCEEDED(VariantCopy(&p_out, &val)))
 		{
 			return true;
 		}
@@ -20,36 +20,40 @@ bool prop_kv_config::get_config_item(const char* p_key, VARIANT& p_out)
 	return false;
 }
 
-bool prop_kv_config::g_is_allowed_type(VARTYPE p_vt)
+int prop_kv_config::g_get_sizeof(VARTYPE p_vt)
 {
 	switch (p_vt)
 	{
+	case VT_BSTR:
+		return -1;
 	case VT_UI1:
 	case VT_I1:
+		return sizeof(BYTE);
 	case VT_I2:
 	case VT_UI2:
 	case VT_BOOL:
+		return sizeof(short);
 	case VT_I4:
 	case VT_UI4:
 	case VT_R4:
 	case VT_INT:
 	case VT_UINT:
+		return sizeof(LONG);
 	case VT_I8:
 	case VT_UI8:
+		return sizeof(LONGLONG);
 	case VT_R8:
 	case VT_CY:
 	case VT_DATE:
-	case VT_BSTR:
-		return true;
+		return sizeof(double);
+	default:
+		return 0;
 	}
-
-	return false;
 }
 
 void prop_kv_config::g_load(t_map& data, stream_reader* reader, abort_callback& abort) throw()
 {
 	t_size count;
-
 	data.remove_all();
 
 	try
@@ -59,49 +63,16 @@ void prop_kv_config::g_load(t_map& data, stream_reader* reader, abort_callback& 
 		for (t_size i = 0; i < count; ++i)
 		{
 			pfc::string8_fast key;
-			_variant_t val;
 			VARTYPE vt;
-			int cbRead = 0;
-
 			reader->read_string(key, abort);
 			reader->read_lendian_t(vt, abort);
 
-			switch (vt)
-			{
-			case VT_UI1:
-			case VT_I1:
-				cbRead = sizeof(BYTE);
-				break;
+			int cbRead = g_get_sizeof(vt);
 
-			case VT_I2:
-			case VT_UI2:
-			case VT_BOOL:
-				cbRead = sizeof(short);
-				break;
-
-			case VT_I4:
-			case VT_UI4:
-			case VT_R4:
-			case VT_INT:
-			case VT_UINT:
-				cbRead = sizeof(LONG);
-				break;
-
-			case VT_I8:
-			case VT_UI8:
-				cbRead = sizeof(LONGLONG);
-				break;
-
-			case VT_R8:
-			case VT_CY:
-			case VT_DATE:
-				cbRead = sizeof(double);
-				break;
-			}
-
+			_variant_t val;
 			val.vt = vt;
 
-			if (cbRead != 0)
+			if (cbRead > 0)
 			{
 				reader->read(&val.bVal, cbRead, abort);
 			}
@@ -128,49 +99,15 @@ void prop_kv_config::g_save(const t_map& data, stream_writer* writer, abort_call
 		{
 			writer->write_string(iter->m_key, abort);
 			writer->write_lendian_t(iter->m_value.vt, abort);
-			int cbWrite = 0;
+			int cbWrite = g_get_sizeof(iter->m_value.vt);
 
-			switch (iter->m_value.vt)
-			{
-			case VT_UI1:
-			case VT_I1:
-				cbWrite = sizeof(BYTE);
-				break;
-
-			case VT_I2:
-			case VT_UI2:
-			case VT_BOOL:
-				cbWrite = sizeof(short);
-				break;
-
-			case VT_I4:
-			case VT_UI4:
-			case VT_R4:
-			case VT_INT:
-			case VT_UINT:
-				cbWrite = sizeof(LONG);
-				break;
-
-			case VT_I8:
-			case VT_UI8:
-				cbWrite = sizeof(LONGLONG);
-				break;
-
-			case VT_R8:
-			case VT_CY:
-			case VT_DATE:
-				cbWrite = sizeof(double);
-				break;
-			}
-
-			if (cbWrite != 0)
+			if (cbWrite > 0)
 			{
 				writer->write(&iter->m_value.bVal, cbWrite, abort);
 			}
-			else if (iter->m_value.vt == VT_BSTR)
+			else if (cbWrite == -1)
 			{
-				string_utf8_from_wide conv = iter->m_value.bstrVal;
-				writer->write_string(conv, abort);
+				writer->write_string(string_utf8_from_wide(iter->m_value.bstrVal), abort);
 			}
 		}
 	}
@@ -189,7 +126,7 @@ void prop_kv_config::save(stream_writer* writer, abort_callback& abort) const th
 
 void prop_kv_config::set_config_item(const char* p_key, const VARIANT& p_val)
 {
-	if (g_is_allowed_type(p_val.vt))
+	if (g_get_sizeof(p_val.vt) != 0)
 	{
 		m_map[p_key] = p_val;
 	}
--- src/ui/config.h
+++ src/ui/config.h
@@ -6,7 +6,7 @@ public:
 	using t_map = pfc::map_t<pfc::string_simple, _variant_t, pfc::comparator_stricmp_ascii>;
 
 	bool get_config_item(const char* p_key, VARIANT& p_out);
-	static bool g_is_allowed_type(VARTYPE p_vt);
+	static int g_get_sizeof(VARTYPE p_vt);
 	static void g_load(t_map& data, stream_reader* reader, abort_callback& abort) throw();
 	static void g_save(const t_map& data, stream_writer* writer, abort_callback& abort) throw();
 	void load(stream_reader* reader, abort_callback& abort) throw();
