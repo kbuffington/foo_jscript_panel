0571856d238a6c8758850f77f82289e12755b11d
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-06-01 00:19:45 +0100

    Update foo_jscript_panel.cpp

--- src/foo_jscript_panel.cpp
+++ src/foo_jscript_panel.cpp
@@ -17,8 +17,6 @@ namespace
 
 	VALIDATE_COMPONENT_FILENAME(JSP_DLL_NAME);
 
-	CAppModule _Module;
-
 	extern "C" BOOL WINAPI DllMain(HINSTANCE ins, DWORD reason, LPVOID lp)
 	{
 		switch (reason)
@@ -30,14 +28,10 @@ namespace
 				if (FAILED(LoadTypeLibEx(string_wide_from_utf8_fast(path), REGKIND_NONE, &g_typelib))) return FALSE;
 
 				Scintilla_RegisterClasses(ins);
-				_Module.Init(nullptr, ins);
 			}
 			break;
 		case DLL_PROCESS_DETACH:
-			{
-				_Module.Term();
-				Scintilla_ReleaseResources();
-			}
+			Scintilla_ReleaseResources();
 			break;
 		}
 		return TRUE;
