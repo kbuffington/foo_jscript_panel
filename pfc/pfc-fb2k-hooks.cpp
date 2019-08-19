36e087992a91d6bfd312ae47d42874e3ca23df59
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-08-19 18:34:56 +0100

    Replace SDK with submodule

--- pfc/pfc-fb2k-hooks.cpp
+++
@@ -1,4 +0,0 @@
-#include "pfc.h"
-#include "pfc-fb2k-hooks.h"
-
-#include "suppress_fb2k_hooks.h"
--- pfc/pfc-fb2k-hooks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace pfc {
-	void crashImpl();
-	BOOL winFormatSystemErrorMessageImpl(pfc::string_base & p_out, DWORD p_code);
-
-	void crashHook();
-	BOOL winFormatSystemErrorMessageHook(pfc::string_base & p_out, DWORD p_code);
-}
