b637fca999ecfefbcaa8c07660cdd41bd856994a
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-10-17 02:46:57 +0100

    Split t_script_info in to own file

---
+++ src/script_info.h
@@ -0,0 +1,66 @@
+#pragma once
+
+struct t_script_info
+{
+	t_script_info() : id(0) {}
+
+	std::vector<std::pair<std::string, std::string>> expand_table =
+	{
+		{ "%fb2k_profile_path%", helpers::get_profile_path().get_ptr() },
+		{ "%fb2k_component_path%", helpers::get_fb2k_component_path().get_ptr() },
+		{ "%fb2k_path%", helpers::get_fb2k_path().get_ptr() },
+	};
+
+	pfc::string8_fast build_info_string() const
+	{
+		pfc::string8_fast ret;
+		ret << JSP_NAME_VERSION " (";
+		if (name.length())
+		{
+			ret << name.c_str();
+		}
+		else
+		{
+			ret << "id:" << id;
+		}
+		if (version.length())
+		{
+			ret << " v" << version.c_str();
+		}
+		if (author.length())
+		{
+			ret << " by " << author.c_str();
+		}
+		ret << ")";
+		return ret;
+	}
+
+	std::string expand_import(t_size idx)
+	{
+		std::string tmp = imports[idx];
+		for (const auto& [key, value] : expand_table)
+		{
+			if (tmp.find(key) == 0)
+			{
+				tmp = value + tmp.substr(key.length());
+				break;
+			}
+		}
+		return tmp;
+	}
+
+	void clear()
+	{
+		imports.clear();
+		name = "";
+		version = "";
+		author = "";
+		id = 0;
+	}
+
+	std::string name;
+	std::string version;
+	std::string author;
+	str_list imports;
+	t_size id;
+};
