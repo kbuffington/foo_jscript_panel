b51820bf803d58cd3941ce6685147976655cfad0
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-11-20 16:48:51 +0000

    Update foo_jscript_panel.h

--- src/foo_jscript_panel.h
+++ src/foo_jscript_panel.h
@@ -16,9 +16,9 @@
 #define JSP_VERSION JSP_VERSION_NUMBER JSP_VERSION_TEST JSP_VERSION_DEBUG_SUFFIX
 #define JSP_NAME_VERSION JSP_NAME " v" JSP_VERSION
 
-#define TO_VARIANT_BOOL(v) ((v) ? (VARIANT_TRUE) : (VARIANT_FALSE))
+#define TO_VARIANT_BOOL(v) (v ? VARIANT_TRUE : VARIANT_FALSE)
 #define TO_BSTR(str) SysAllocString(string_wide_from_utf8_fast(str))
-#define TO_INT(num) num == SIZE_MAX ? -1 : static_cast<int>(num)
+#define TO_INT(num) (num == SIZE_MAX ? -1 : static_cast<int>(num))
 
 #define GET_PTR(X, Y) \
 	X->get__ptr(reinterpret_cast<void**>(&Y)); \
