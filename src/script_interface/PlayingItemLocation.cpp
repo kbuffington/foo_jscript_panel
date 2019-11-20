989f3a1688f80e067582c836ef580c118445a723
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-11-20 14:06:11 +0000

    Update PlayingItemLocation.cpp

--- src/script_interface/PlayingItemLocation.cpp
+++ src/script_interface/PlayingItemLocation.cpp
@@ -15,7 +15,7 @@ STDMETHODIMP PlayingItemLocation::get_PlaylistIndex(int* p)
 {
 	if (!p) return E_POINTER;
 
-	*p = m_isValid ? m_playlistIndex : -1;
+	*p = m_isValid ? static_cast<int>(m_playlistIndex) : -1;
 	return S_OK;
 }
 
@@ -23,6 +23,6 @@ STDMETHODIMP PlayingItemLocation::get_PlaylistItemIndex(int* p)
 {
 	if (!p) return E_POINTER;
 
-	*p = m_isValid ? m_playlistItemIndex : -1;
+	*p = m_isValid ? static_cast<int>(m_playlistItemIndex) : -1;
 	return S_OK;
 }
