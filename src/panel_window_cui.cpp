6e4585ffc0bd5bf2e38a297729cc9e056040d5d4
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2019-12-20 08:44:23 +0000

    Anon

--- src/panel_window_cui.cpp
+++ src/panel_window_cui.cpp
@@ -1,192 +1,195 @@
 #include "stdafx.h"
 #include "panel_window.h"
 
-class panel_window_cui : public panel_window, public uie::container_ui_extension, public cui::fonts::common_callback, public cui::colours::common_callback
+namespace
 {
-protected:
-	COLORREF get_colour_ui(size_t type) override
+	class panel_window_cui : public panel_window, public uie::container_ui_extension, public cui::fonts::common_callback, public cui::colours::common_callback
 	{
-		const auto t = static_cast<cui::colours::colour_identifier_t>(type);
-		if (t <= cui::colours::colour_active_item_frame)
+	protected:
+		COLORREF get_colour_ui(size_t type) override
 		{
-			cui::colours::helper helper(pfc::guid_null);
-			return helper.get_colour(t);
-		}
-		return 0;
-	}
-
-	HWND create_or_transfer_window(HWND parent, const uie::window_host_ptr& host, const ui_helpers::window_position_t& p_position) override
-	{
-		if (m_hwnd)
-		{
-			m_hwnd.ShowWindow(SW_HIDE);
-			m_hwnd.SetParent(parent);
-			m_host->relinquish_ownership(m_hwnd);
-			m_host = host;
-
-			m_hwnd.SetWindowPos(nullptr, p_position.x, p_position.y, p_position.cx, p_position.cy, SWP_NOZORDER);
-		}
-		else
-		{
-			m_host = host;
-			create(parent, this, p_position);
-		}
-
-		pfc::string8_fast name;
-		uGetClassName(parent, name);
-		m_supports_transparency = name.equals("PSSWindowContainer") || name.equals("ReBarWindow32");
-
-		return m_hwnd;
-	}
-
-	HWND get_wnd() const override
-	{
-		return m_hwnd;
-	}
-
-	IGdiFont* get_font_ui(size_t type) override
-	{
-		IGdiFont* ret = nullptr;
-
-		const auto t = static_cast<cui::fonts::font_type_t>(type);
-		if (t <= cui::fonts::font_type_labels)
-		{
-			HFONT hFont = static_api_ptr_t<cui::fonts::manager>()->get_font(t);
-			if (hFont)
+			const auto t = static_cast<cui::colours::colour_identifier_t>(type);
+			if (t <= cui::colours::colour_active_item_frame)
 			{
-				auto font = new Gdiplus::Font(m_hdc, hFont);
-				if (helpers::ensure_gdiplus_object(font))
+				cui::colours::helper helper(pfc::guid_null);
+				return helper.get_colour(t);
+			}
+			return 0;
+		}
+
+		HWND create_or_transfer_window(HWND parent, const uie::window_host_ptr& host, const ui_helpers::window_position_t& p_position) override
+		{
+			if (m_hwnd)
+			{
+				m_hwnd.ShowWindow(SW_HIDE);
+				m_hwnd.SetParent(parent);
+				m_host->relinquish_ownership(m_hwnd);
+				m_host = host;
+
+				m_hwnd.SetWindowPos(nullptr, p_position.x, p_position.y, p_position.cx, p_position.cy, SWP_NOZORDER);
+			}
+			else
+			{
+				m_host = host;
+				create(parent, this, p_position);
+			}
+
+			pfc::string8_fast name;
+			uGetClassName(parent, name);
+			m_supports_transparency = name.equals("PSSWindowContainer") || name.equals("ReBarWindow32");
+
+			return m_hwnd;
+		}
+
+		HWND get_wnd() const override
+		{
+			return m_hwnd;
+		}
+
+		IGdiFont* get_font_ui(size_t type) override
+		{
+			IGdiFont* ret = nullptr;
+
+			const auto t = static_cast<cui::fonts::font_type_t>(type);
+			if (t <= cui::fonts::font_type_labels)
+			{
+				HFONT hFont = static_api_ptr_t<cui::fonts::manager>()->get_font(t);
+				if (hFont)
 				{
-					ret = new com_object_impl_t<GdiFont>(font, hFont);
+					auto font = new Gdiplus::Font(m_hdc, hFont);
+					if (helpers::ensure_gdiplus_object(font))
+					{
+						ret = new com_object_impl_t<GdiFont>(font, hFont);
+					}
+					else
+					{
+						if (font) delete font;
+						font = nullptr;
+					}
 				}
-				else
+			}
+			return ret;
+		}
+
+		LRESULT on_message(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) override
+		{
+			switch (msg)
+			{
+			case WM_SETCURSOR:
+				return 1;
+			case WM_ERASEBKGND:
+				if (is_transparent()) m_hwnd.PostMessage(jsp::uwm_refreshbk);
+				return 1;
+			case WM_CONTEXTMENU:
+				on_context_menu(lp);
+				return 1;
+			case WM_CREATE:
+				try
 				{
-					if (font) delete font;
-					font = nullptr;
+					static_api_ptr_t<cui::fonts::manager>()->register_common_callback(this);
+					static_api_ptr_t<cui::colours::manager>()->register_common_callback(this);
 				}
+				catch (...) {}
+				break;
+			case WM_DESTROY:
+				try
+				{
+					static_api_ptr_t<cui::fonts::manager>()->deregister_common_callback(this);
+					static_api_ptr_t<cui::colours::manager>()->deregister_common_callback(this);
+				}
+				catch (...) {}
+				break;
+			case WM_SYSKEYDOWN:
+			case WM_KEYDOWN:
+				if (uie::window::g_process_keydown_keyboard_shortcuts(wp))
+					return 0;
+				break;
 			}
-		}
-		return ret;
-	}
 
-	LRESULT on_message(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) override
-	{
-		switch (msg)
+			if (handle_message(hwnd, msg, wp, lp)) return 0;
+			return uDefWindowProc(hwnd, msg, wp, lp);
+		}
+
+		bool have_config_popup() const override
 		{
-		case WM_SETCURSOR:
-			return 1;
-		case WM_ERASEBKGND:
-			if (is_transparent()) m_hwnd.PostMessage(jsp::uwm_refreshbk);
-			return 1;
-		case WM_CONTEXTMENU:
-			on_context_menu(lp);
-			return 1;
-		case WM_CREATE:
-			try
-			{
-				static_api_ptr_t<cui::fonts::manager>()->register_common_callback(this);
-				static_api_ptr_t<cui::colours::manager>()->register_common_callback(this);
-			}
-			catch (...) {}
-			break;
-		case WM_DESTROY:
-			try
-			{
-				static_api_ptr_t<cui::fonts::manager>()->deregister_common_callback(this);
-				static_api_ptr_t<cui::colours::manager>()->deregister_common_callback(this);
-			}
-			catch (...) {}
-			break;
-		case WM_SYSKEYDOWN:
-		case WM_KEYDOWN:
-			if (uie::window::g_process_keydown_keyboard_shortcuts(wp))
-				return 0;
-			break;
+			return true;
 		}
 
-		if (handle_message(hwnd, msg, wp, lp)) return 0;
-		return uDefWindowProc(hwnd, msg, wp, lp);
-	}
-
-	bool have_config_popup() const override
-	{
-		return true;
-	}
-
-	bool is_available(const uie::window_host_ptr& p) const override
-	{
-		return true;
-	}
-
-	bool show_config_popup(HWND parent) override
-	{
-		show_configure_popup(parent);
-		return true;
-	}
-
-	class_data& get_class_data() const override
-	{
-		__implement_get_class_data_ex(jsp::class_name, L"", false, 0, WS_CHILD | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, 0, CS_DBLCLKS);
-	}
-
-	const GUID& get_extension_guid() const override
-	{
-		return jsp::guids::window_cui;
-	}
-
-	size_t get_type() const override
-	{
-		return uie::type_toolbar | uie::type_panel;
-	}
-
-	void destroy_window() override
-	{
-		destroy();
-		m_host.release();
-	}
-
-	void get_category(pfc::string_base& out) const override
-	{
-		out = "Panels";
-	}
-
-	void get_config(stream_writer* writer, abort_callback& abort) const override
-	{
-		save_config(writer, abort);
-	}
-
-	void get_name(pfc::string_base& out) const override
-	{
-		out = jsp::component_name;
-	}
-
-	void on_bool_changed(size_t mask) const override {}
-
-	void on_colour_changed(size_t mask) const override
-	{
-		script_invoke(callback_id::on_colours_changed);
-	}
-
-	void on_font_changed(size_t mask) const override
-	{
-		script_invoke(callback_id::on_font_changed);
-	}
-
-	void set_config(stream_reader* reader, size_t size, abort_callback& abort) override
-	{
-		load_config(reader, size, abort);
-	}
-
-	void notify_size_limit_changed() override
-	{
-		if (m_host.is_valid())
+		bool is_available(const uie::window_host_ptr& p) const override
 		{
-			m_host->on_size_limit_change(m_hwnd, uie::size_limit_all);
+			return true;
 		}
-	}
 
-private:
-	uie::window_host_ptr m_host;
-};
+		bool show_config_popup(HWND parent) override
+		{
+			show_configure_popup(parent);
+			return true;
+		}
 
-static uie::window_factory<panel_window_cui> g_panel_window_cui;
+		class_data& get_class_data() const override
+		{
+			__implement_get_class_data_ex(jsp::class_name, L"", false, 0, WS_CHILD | WS_CLIPSIBLINGS | WS_CLIPCHILDREN, 0, CS_DBLCLKS);
+		}
+
+		const GUID& get_extension_guid() const override
+		{
+			return jsp::guids::window_cui;
+		}
+
+		size_t get_type() const override
+		{
+			return uie::type_toolbar | uie::type_panel;
+		}
+
+		void destroy_window() override
+		{
+			destroy();
+			m_host.release();
+		}
+
+		void get_category(pfc::string_base& out) const override
+		{
+			out = "Panels";
+		}
+
+		void get_config(stream_writer* writer, abort_callback& abort) const override
+		{
+			save_config(writer, abort);
+		}
+
+		void get_name(pfc::string_base& out) const override
+		{
+			out = jsp::component_name;
+		}
+
+		void on_bool_changed(size_t mask) const override {}
+
+		void on_colour_changed(size_t mask) const override
+		{
+			script_invoke(callback_id::on_colours_changed);
+		}
+
+		void on_font_changed(size_t mask) const override
+		{
+			script_invoke(callback_id::on_font_changed);
+		}
+
+		void set_config(stream_reader* reader, size_t size, abort_callback& abort) override
+		{
+			load_config(reader, size, abort);
+		}
+
+		void notify_size_limit_changed() override
+		{
+			if (m_host.is_valid())
+			{
+				m_host->on_size_limit_change(m_hwnd, uie::size_limit_all);
+			}
+		}
+
+	private:
+		uie::window_host_ptr m_host;
+	};
+
+	static uie::window_factory<panel_window_cui> g_panel_window_cui;
+}
--- src/panel_window_dui.cpp
+++ src/panel_window_dui.cpp
@@ -1,177 +1,180 @@
 #include "stdafx.h"
 #include "panel_window.h"
 
-class panel_window_dui : public panel_window, public ui_element_instance, public CWindowImpl<panel_window_dui>
+namespace
 {
-public:
-	DECLARE_WND_CLASS_EX(jsp::class_name, CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS, -1);
-
-	panel_window_dui(ui_element_config::ptr cfg, ui_element_instance_callback::ptr callback) : m_callback(callback)
+	class panel_window_dui : public panel_window, public ui_element_instance, public CWindowImpl<panel_window_dui>
 	{
-		m_instance_type = instance_type::dui;
-		set_configuration(cfg);
-	}
+	public:
+		DECLARE_WND_CLASS_EX(jsp::class_name, CS_VREDRAW | CS_HREDRAW | CS_DBLCLKS, -1);
 
-	static GUID g_get_guid()
-	{
-		return jsp::guids::window_dui;
-	}
-
-	static GUID g_get_subclass()
-	{
-		return ui_element_subclass_utility;
-	}
-
-	static pfc::string8_fast g_get_description()
-	{
-		return "Customisable panel with JScript scripting support.";
-	}
-
-	static ui_element_config::ptr g_get_default_configuration()
-	{
-		ui_element_config_builder builder;
-		return builder.finish(g_get_guid());
-	}
-
-	static void g_get_name(pfc::string_base& out)
-	{
-		out = jsp::component_name;
-	}
-
-	BOOL ProcessWindowMessage(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp, LRESULT& res, DWORD) override
-	{
-		res = 0;
-
-		switch (msg)
+		panel_window_dui(ui_element_config::ptr cfg, ui_element_instance_callback::ptr callback) : m_callback(callback)
 		{
-		case WM_SETCURSOR:
-		case WM_ERASEBKGND:
-			res = 1;
-			return TRUE;
-		case WM_CONTEXTMENU:
-			res = 1;
-			if (m_callback->is_edit_mode_enabled()) return FALSE;
-			on_context_menu(lp);
-			return TRUE;
-		case WM_RBUTTONUP:
-		case WM_RBUTTONDOWN:
-		case WM_RBUTTONDBLCLK:
-			if (m_callback->is_edit_mode_enabled()) return FALSE;
-			break;
-		case WM_GETDLGCODE:
-			res = DLGC_WANTALLKEYS;
-			return TRUE;
+			m_instance_type = instance_type::dui;
+			set_configuration(cfg);
 		}
 
-		if (handle_message(hwnd, msg, wp, lp)) return TRUE;
-		return FALSE;
-	}
-
-	COLORREF get_colour_ui(size_t type) override
-	{
-		return type < jsp::guids::colours.size() ? m_callback->query_std_color(*jsp::guids::colours[type]) : 0;
-	}
-
-	GUID get_guid() override
-	{
-		return g_get_guid();
-	}
-
-	GUID get_subclass() override
-	{
-		return g_get_subclass();
-	}
-
-	HWND get_wnd() override
-	{
-		return m_hwnd;
-	}
-
-	IGdiFont* get_font_ui(size_t type) override
-	{
-		IGdiFont* ret = nullptr;
-
-		if (type < jsp::guids::fonts.size())
+		static GUID g_get_guid()
 		{
-			HFONT hFont = m_callback->query_font_ex(*jsp::guids::fonts[type]);
-			if (hFont)
+			return jsp::guids::window_dui;
+		}
+
+		static GUID g_get_subclass()
+		{
+			return ui_element_subclass_utility;
+		}
+
+		static pfc::string8_fast g_get_description()
+		{
+			return "Customisable panel with JScript scripting support.";
+		}
+
+		static ui_element_config::ptr g_get_default_configuration()
+		{
+			ui_element_config_builder builder;
+			return builder.finish(g_get_guid());
+		}
+
+		static void g_get_name(pfc::string_base& out)
+		{
+			out = jsp::component_name;
+		}
+
+		BOOL ProcessWindowMessage(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp, LRESULT& res, DWORD) override
+		{
+			res = 0;
+
+			switch (msg)
 			{
-				auto font = new Gdiplus::Font(m_hdc, hFont);
-				if (helpers::ensure_gdiplus_object(font))
+			case WM_SETCURSOR:
+			case WM_ERASEBKGND:
+				res = 1;
+				return TRUE;
+			case WM_CONTEXTMENU:
+				res = 1;
+				if (m_callback->is_edit_mode_enabled()) return FALSE;
+				on_context_menu(lp);
+				return TRUE;
+			case WM_RBUTTONUP:
+			case WM_RBUTTONDOWN:
+			case WM_RBUTTONDBLCLK:
+				if (m_callback->is_edit_mode_enabled()) return FALSE;
+				break;
+			case WM_GETDLGCODE:
+				res = DLGC_WANTALLKEYS;
+				return TRUE;
+			}
+
+			if (handle_message(hwnd, msg, wp, lp)) return TRUE;
+			return FALSE;
+		}
+
+		COLORREF get_colour_ui(size_t type) override
+		{
+			return type < jsp::guids::colours.size() ? m_callback->query_std_color(*jsp::guids::colours[type]) : 0;
+		}
+
+		GUID get_guid() override
+		{
+			return g_get_guid();
+		}
+
+		GUID get_subclass() override
+		{
+			return g_get_subclass();
+		}
+
+		HWND get_wnd() override
+		{
+			return m_hwnd;
+		}
+
+		IGdiFont* get_font_ui(size_t type) override
+		{
+			IGdiFont* ret = nullptr;
+
+			if (type < jsp::guids::fonts.size())
+			{
+				HFONT hFont = m_callback->query_font_ex(*jsp::guids::fonts[type]);
+				if (hFont)
 				{
-					ret = new com_object_impl_t<GdiFont>(font, hFont, false);
-				}
-				else
-				{
-					if (font) delete font;
-					font = nullptr;
+					auto font = new Gdiplus::Font(m_hdc, hFont);
+					if (helpers::ensure_gdiplus_object(font))
+					{
+						ret = new com_object_impl_t<GdiFont>(font, hFont, false);
+					}
+					else
+					{
+						if (font) delete font;
+						font = nullptr;
+					}
 				}
 			}
+			return ret;
 		}
-		return ret;
-	}
 
-	bool edit_mode_context_menu_test(const POINT& p_point, bool p_fromkeyboard) override
-	{
-		return true;
-	}
-
-	ui_element_config::ptr get_configuration() override
-	{
-		ui_element_config_builder builder;
-		save_config(&builder.m_stream, fb2k::noAbort);
-		return builder.finish(g_get_guid());
-	}
-
-	void edit_mode_context_menu_build(const POINT& p_point, bool p_fromkeyboard, HMENU p_menu, size_t p_id_base) override
-	{
-		build_context_menu(p_menu, p_id_base);
-	}
-
-	void edit_mode_context_menu_command(const POINT& p_point, bool p_fromkeyboard, size_t p_id, size_t p_id_base) override
-	{
-		execute_context_menu_command(p_id, p_id_base);
-	}
-
-	void initialize_window(HWND parent)
-	{
-		Create(parent);
-	}
-
-	void notify(const GUID& p_what, size_t p_param1, const void* p_param2, size_t p_param2size) override
-	{
-		if (p_what == ui_element_notify_colors_changed)
+		bool edit_mode_context_menu_test(const POINT& p_point, bool p_fromkeyboard) override
 		{
-			script_invoke(callback_id::on_colours_changed);
+			return true;
 		}
-		else if (p_what == ui_element_notify_font_changed)
+
+		ui_element_config::ptr get_configuration() override
 		{
-			script_invoke(callback_id::on_font_changed);
+			ui_element_config_builder builder;
+			save_config(&builder.m_stream, fb2k::noAbort);
+			return builder.finish(g_get_guid());
 		}
-	}
 
-	void notify_size_limit_changed() override
-	{
-		m_callback->on_min_max_info_change();
-	}
-
-	void set_configuration(ui_element_config::ptr data) override
-	{
-		ui_element_config_parser parser(data);
-
-		load_config(&parser.m_stream, parser.get_remaining(), fb2k::noAbort);
-
-		// FIX: If window already created, DUI won't destroy it and create it again.
-		if (m_hwnd)
+		void edit_mode_context_menu_build(const POINT& p_point, bool p_fromkeyboard, HMENU p_menu, size_t p_id_base) override
 		{
-			update_script();
+			build_context_menu(p_menu, p_id_base);
 		}
-	}
 
-private:
-	ui_element_instance_callback::ptr m_callback;
-};
+		void edit_mode_context_menu_command(const POINT& p_point, bool p_fromkeyboard, size_t p_id, size_t p_id_base) override
+		{
+			execute_context_menu_command(p_id, p_id_base);
+		}
 
-class my_ui_element_impl : public ui_element_impl<panel_window_dui> {};
+		void initialize_window(HWND parent)
+		{
+			Create(parent);
+		}
 
-FB2K_SERVICE_FACTORY(my_ui_element_impl);
+		void notify(const GUID& p_what, size_t p_param1, const void* p_param2, size_t p_param2size) override
+		{
+			if (p_what == ui_element_notify_colors_changed)
+			{
+				script_invoke(callback_id::on_colours_changed);
+			}
+			else if (p_what == ui_element_notify_font_changed)
+			{
+				script_invoke(callback_id::on_font_changed);
+			}
+		}
+
+		void notify_size_limit_changed() override
+		{
+			m_callback->on_min_max_info_change();
+		}
+
+		void set_configuration(ui_element_config::ptr data) override
+		{
+			ui_element_config_parser parser(data);
+
+			load_config(&parser.m_stream, parser.get_remaining(), fb2k::noAbort);
+
+			// FIX: If window already created, DUI won't destroy it and create it again.
+			if (m_hwnd)
+			{
+				update_script();
+			}
+		}
+
+	private:
+		ui_element_instance_callback::ptr m_callback;
+	};
+
+	class my_ui_element_impl : public ui_element_impl<panel_window_dui> {};
+
+	FB2K_SERVICE_FACTORY(my_ui_element_impl);
+}
